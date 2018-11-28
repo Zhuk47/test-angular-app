96f27b475f972147f36dbe7ef2dad75df58d645a
Aleksandr Zhukovskiy <aleksandr.zhukovskiy@appus.me>
2018-11-28 15:37:27 +0200

    [+] task & task-details components

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,9 @@
+import {Task} from './task';
+
+export const TASKS: Task[] = [
+    {id: 1, name: 'Log-in', description: 'Log in to the system'},
+    {id: 2, name: 'Working', description: 'Work on your\'s tickets'},
+    {id: 3, name: 'Dinner', description: 'Take your dinner'},
+    {id: 4, name: 'Completing', description: 'Complete your tickets'},
+    {id: 5, name: 'Log-out', description: 'Log out from the system'},
+];
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {Task} from '../task';
+import {TASKS} from '../mock-tasks';
+
+@Component({
+    selector: 'app-tasks',
+    templateUrl: './tasks.component.html',
+    styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+    tasks = TASKS;
+    selectedTask: Task;
+
+    onSelect(task: Task): void {
+        this.selectedTask = task;
+    }
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
