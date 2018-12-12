a7398b09f795c480467b5d5fdae27a0a91e57904
Aleksandr Zhukovskiy <aleksandr.zhukovskiy@appus.me>
2018-12-12 20:14:48 +0200

    [+] messages component + dashboard sceleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {TasksComponent} from './tasks/tasks.component';
 import {TaskDetailComponent} from './task-detail/task-detail.component';
-import { MessagesComponent } from './messages/messages.component';
+import {MessagesComponent} from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         TasksComponent,
         TaskDetailComponent,
-        MessagesComponent
+        MessagesComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Task} from './task';
 import {TASKS} from './mock-tasks';
 import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
     providedIn: 'root'
@@ -9,9 +10,11 @@ import {Observable, of} from 'rxjs';
 export class TaskService {
 
     getTasks(): Observable<Task[]> {
+        // TODO: send the message _after_ fetching the heroes
+        this.messageService.add('TaskService: fetched tasks');
         return of(TASKS);
     }
 
-    constructor() {
+    constructor(private messageService: MessageService) {
     }
 }
